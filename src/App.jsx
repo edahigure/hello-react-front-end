28bb10eb4f3d499182bd1ce9efda738c34d066ff
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-09-23 16:10:52 -0600

    Install linter and fix linters

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import './App.css'
-
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Greetings from './pages/Greetings';
 
--- src/pages/Greetings.jsx
+++ src/pages/Greetings.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import  { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchMessages } from '../redux/messages/messagesSlice';
 import Greeting from '../components/Greeting';
